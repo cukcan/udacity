6f0d20f4462717c3b4ed89b0b549904bb34df27b
Ibrahim <17607802+cukcan@users.noreply.github.com>
2020-11-20 06:28:37 +0300

    Create moneyprice.js

---
+++ moneyprice.js
@@ -0,0 +1,10 @@
+var money = 100.50;
+var price = 100.50;
+
+if (money > price) {
+  console.log('You paid extra, here\'s your change.');
+} else if (money < price) {
+  console.log('That\'s not enough, you still owe me money.');
+} else {
+  console.log('You paid the exact amount, have a nice day!');
+}
