537d0e675b872840d6d04b40704cdad57ec98b17
Ibrahim <17607802+cukcan@users.noreply.github.com>
2020-11-26 18:12:02 +0300

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+var x = 1;
+while (x <= 100) {
+    if (x % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (x % 5 === 0) {
+        console.log("Buzz");
+    } else if (x % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(x);
+    }
+    x++;
+}
