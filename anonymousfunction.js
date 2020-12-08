43e1f6e3d2e4e4ca7c8245304d585414dab7f580
Ibrahim <17607802+cukcan@users.noreply.github.com>
2020-12-08 10:11:55 +0300

    Create anonymousfunction.js

---
+++ anonymousfunction.js
@@ -0,0 +1,9 @@
+var x = "";
+var laugh = function(num) {
+    for (i = 0; i < num; i++) {
+        x += "ha"; 
+    }
+    return x + "!";
+}/* finish the function expression */
+
+console.log(laugh(10));
